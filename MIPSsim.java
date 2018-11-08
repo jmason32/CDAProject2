9c4ab5cbbd389b0ada686a58714edacc25c6c705
Jordan Mason <jmason32@gmail.com>
2018-11-08 12:47:25 -0500

    2

--- MIPSsim.java
+++ MIPSsim.java
@@ -1,10 +1,17 @@
-
+import java.io.*;
+import java.util.*;
 
 
 class MIPSsim {
 	
+	//Have a list/
+	public void registerPrint(){
+
+	}
+
 
 	public static void main(String[] args) {
+		Scanner scanner; 
 		
 	}
 }
